9e65c06ee75286f259ef3f707eb9685838894e88
BRANDEDXKING <147673119+BRANDEDXKING@users.noreply.github.com>
2023-10-20 06:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # TEAM DAXX ALL COPYRIGHT ©️
 class Config:
-    API_ID = "20136640"
-    API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6437116754:AAFlq2FZ3E-KLM4aOIFT7iothxMjygwa9n4"
+    API_ID = "21523368"
+    API_HASH = "a1ea11b0091228842eac7b1f252f3b10"
+    TOKEN = "6651057296:AAEx--eHEkwvUZTCEYacml4kEKVrrojJENE"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
