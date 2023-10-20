53a1305995fd5a9d6e967bc90cc69becc9d2fd3f
BRANDEDXKING <147673119+BRANDEDXKING@users.noreply.github.com>
2023-10-20 07:30:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 class Config:
     API_ID = "21523368"
     API_HASH = "a1ea11b0091228842eac7b1f252f3b10"
-    TOKEN = "6651057296:AAEx--eHEkwvUZTCEYacml4kEKVrrojJENE"
+    TOKEN = "6430193290:AAFPdyQp5pAfk1e7R_KpbTrKfnRxgt5rUX8"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
